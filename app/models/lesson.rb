eb542dcf89bccfcfb52768e9ed86fd20c17f4fd5
Jeffruy Examene <Examene7007@gmail.com>
2016-04-10 19:32:51 +0000

    Hookup next lesson button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -7,4 +7,11 @@ class Lesson < ActiveRecord::Base
 
 	#order lesson with same section id
 	ranks :row_order, :with_same => :section_id
+
+	def next_lesson
+		# look for section of lesson filter out lesson not ahter current lesson in section
+		lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+
+		return lesson
+	end
 end
